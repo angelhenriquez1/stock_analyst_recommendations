2180c58cb9fb978236f7020423d3fe8cf3866cf8
Angel Henriquez <angelhenriquez@Angels-MacBook-Pro.local>
2020-12-03 08:53:02 -0800

    FULL LIST OF STOCK SYMBOLS

---
+++ stock_symbol_list.R
@@ -0,0 +1,131 @@
+# larger stock symbol dataset
+
+# list of all stock symbols traded
+
+nyse <- read.csv("~/Stock Average Price Projection/nyse.csv", stringsAsFactors=FALSE)
+amex <- read_csv("Stock Average Price Projection/amex.csv", skip = 1)
+nasdaq <- read.csv("~/Stock Average Price Projection/nasdaq.csv", stringsAsFactors=FALSE)
+
+stocks <- rbind(nasdaq, nyse)
+names(stocks)[8] <- "Summary Quote"
+stocks <- stocks[1:8]
+amex <- amex[1:8]
+
+stocks <- rbind(stocks, amex)
+stocks <- stocks$Symbol
+stocks <- sort(stocks)
+stocks <- as.data.frame(stocks)
+stocks$stocks <- gsub("\\..*","",stocks$stocks)
+stocks <- unique(stocks$stocks)
+stock_list <- as.list(stocks)
+#stocks <- as.data.frame(stocks)
+
+#Financhill recs####
+financhill <- function(stock_sign) {
+      
+      financhill_url <- paste0("https://financhill.com/stock-forecast/", stock_sign, "-stock-prediction")
+      url <- read_html(financhill_url)
+      
+      words <- url %>%
+         html_nodes("h4") %>%
+         html_text() %>%
+         as.data.frame()
+      
+      rec <- words[c(12),]
+      rec <- as.character(rec)
+      rec <- unlist(strsplit(rec, split = " ", fixed = TRUE))
+      rec <- tail(rec, n=1)
+      rec <- as.character(rec)
+      rec <- unlist(strsplit(rec, split = "\t", fixed = TRUE))
+      rec <- rec[1]
+      rec <- unlist(strsplit(rec, split = " ", fixed = TRUE))
+      
+      buy <- paste0(stock_sign, " is a Buy")
+      result <- ifelse(rec == "Buy", print(buy), "")
+
+   }
+
+# financhill
+  for (i in stock_list){
+   
+      new_element <- financhill(i)
+      list_[[length(list_) + 1]] <- new_element
+
+}
+
+#Barchart####
+bar_chart_rec <- function(stock_sign){
+   
+   bar_chart <- function(stock_sign){
+      
+      url <-  paste0("https://www.barchart.com/stocks/quotes/", stock_sign)
+      url <- read_html(url)
+      
+      words <- url %>%
+         html_nodes("a") %>%
+         html_text() %>%
+         str_squish() %>%
+         as.data.frame()
+      
+      names(words)[1] = "rec"
+      
+      # removing blank rows
+      words1 <- words[!apply(is.na(words) | words == "", 1, all),]
+      words1 <- as.data.frame(words1)
+      
+      strong_buy <- subset(words1$words1, words1$words1=="Strong buy")
+      strong_buy <- as.character(strong_buy)
+      
+      buy <- subset(words1$words1, words1$words1=="buy")
+      buy <- as.character(buy)
+      
+      weak_buy <- subset(words1$words1, words1$words1=="Weak buy")
+      weak_buy <- as.character(weak_buy)
+      
+      hold <- subset(words1$words1, words1$words1=="hold")
+      hold <- as.character(hold)
+      
+      weak_sell <- subset(words1$words1, words1$words1=="Weak sell")
+      weak_sell <- as.character(weak_sell)
+      
+      sell <- subset(words1$words1, words1$words1=="sell")
+      sell <- as.character(sell)
+      
+      strong_sell <- subset(words1$words1, words1$words1=="Strong sell")
+      strong_sell <- as.character(strong_sell)
+      
+      opinion <- rbind(strong_buy, buy, all = TRUE)
+      opinion <- rbind(opinion, weak_buy, all = TRUE)
+      opinion <- rbind(opinion, hold, all=TRUE)
+      opinion <- rbind(opinion, weak_sell, all = TRUE)
+      opinion <- rbind(opinion, sell, all = TRUE)
+      opinion <- rbind(opinion, strong_sell, all = TRUE)
+      
+      opinion <- as.data.frame(opinion)
+      
+      opinion <- opinion %>% 
+         filter(!grepl('TRUE', opinion$V1))
+      
+      print("Barchart Recommendation")
+      ifelse(opinion == "Strong buy" | opinion == "Buy", print(opinion), print(""))
+      
+      #print(opinion)
+      
+   }
+   stock_analysis <- function(stock_sign){
+      
+      print(stock_sign)
+      ifelse(bar_chart(stock_sign) == "Strong buy" , print(bar_chart(stock_sign)), print(""))
+      ifelse(bar_chart(stock_sign) == "Buy" , print(bar_chart(stock_sign)), print(""))
+      
+   }
+   
+   stock_analysis(stock_sign)
+}
+
+for (i in stock_list){
+   new_element <- bar_chart_rec(i)
+   tryCatch(
+   list_[[length(list_) + 1]] <- new_element
+   )
+}
